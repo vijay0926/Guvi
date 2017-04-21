d2ac9eb0cc20e2a9166f05e0dffabad0c27b009c
vijay0926 <vijayarajan2696@gmail.com>
2017-04-21 15:12:13 +0530

    Add files via upload

---
+++ rev.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main(){
+char s[100];
+printf("Enter the string: ");
+gets(s);
+printf("reverse string is :%s",strrev(s));
+getch();
+}
+
